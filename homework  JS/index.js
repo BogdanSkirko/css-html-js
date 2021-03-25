e7b9cfbe1565695ca52bb7548cceecca2021477b
Bohdan13011991 <Bogdanskirko@gmail.com>
2021-03-25 17:15:26 +0200

    Javascript

---
+++ homework  JS/index.js
@@ -0,0 +1,126 @@
+//- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+let one = 'Hello'
+let two = 'owu'
+let three = 'com'
+let four = 'ua'
+let five = '1'
+let six = '10'
+let seven = '-999'
+let eight = '123'
+let Pi = '3.14'
+let ten = '2.7'
+let eleven = '16'
+let twelve = true
+let thirteen = false
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+console.log(one);
+console.log(two);
+console.log(three);
+console.log(four);
+console.log(five);
+console.log(six);
+console.log(seven);
+console.log(eight);
+console.log(Pi);
+console.log(ten);
+console.log(eleven);
+console.log(twelve);
+console.log(thirteen);
+
+alert(one);
+alert(two);
+alert(three);
+alert(four);
+alert(five);
+alert(six);
+alert(seven);
+alert(eight);
+alert(Pi);
+alert(ten);
+alert(eleven);
+alert(twelve);
+alert(twelve);
+alert(thirteen);
+
+document.write(one);
+document.write(two);
+document.write(three);
+document.write(four);
+document.write(five);
+document.write(six);
+document.write(seven);
+document.write(eight);
+document.write(Pi);
+document.write(ten);
+document.write(eleven);
+document.write(twelve);
+document.write(thirteen);
+//- Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
+one = 'Helloworld'
+two = 'okten'
+three = 'ua'
+four = 'eu'
+five = 'Spain'
+six = 'England'
+seven = 'Scotland'
+eight = 'Usa'
+Pi = '3.1454654645'
+ten = '10'
+eleven = '11'
+twelve = false
+thirteen = true
+//Вивести кожну змінну за допомогою: console.log , alert, document.write
+console.log(one);
+console.log(two);
+console.log(three);
+console.log(four);
+console.log(five);
+console.log(six);
+console.log(seven);
+console.log(eight);
+console.log(Pi);
+console.log(ten);
+console.log(eleven);
+console.log(twelve);
+console.log(thirteen);
+
+alert(one);
+alert(two);
+alert(three);
+alert(four);
+alert(five);
+alert(six);
+alert(seven);
+alert(eight);
+alert(Pi);
+alert(ten);
+alert(eleven);
+alert(twelve);
+alert(twelve);
+alert(thirteen);
+
+document.write(one);
+document.write(two);
+document.write(three);
+document.write(four);
+document.write(five);
+document.write(six);
+document.write(seven);
+document.write(eight);
+document.write(Pi);
+document.write(ten);
+document.write(eleven);
+document.write(twelve);
+document.write(thirteen);
+//- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+let yourname = prompt('Ваше ім*я?')
+let fathername = prompt('Ім*я Вашого батька')
+let UserAge = prompt('Ваш Вік?')
+console.log(yourname);
+console.log(fathername);
+console.log(UserAge);
+
+
+
+
+
