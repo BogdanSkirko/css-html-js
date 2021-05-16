5535e340e724b5e4f690540b8c5a1de5df66cb9d
Bohdan13011991 <Bogdanskirko@gmail.com>
2021-05-16 12:34:30 +0300

    homework 3 js

--- homework  JS/index.js
+++ homework  JS/index.js
@@ -1,204 +1,328 @@
-// //- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
-let one = 'Hello'
-let two = 'owu'
-let three = 'com'
-let four = 'ua'
-let five = '1'
-let six = '10'
-let seven = '-999'
-let eight = '123'
-let Pi = '3.14'
-let ten = '2.7'
-let eleven = '16'
-let twelve = true
-let thirteen = false
-// // Вивести кожну змінну за допомогою: console.log , alert, document.write
-console.log(one);
-console.log(two);
-console.log(three);
-console.log(four);
-console.log(five);
-console.log(six);
-console.log(seven);
-console.log(eight);
-console.log(Pi);
-console.log(ten);
-console.log(eleven);
-console.log(twelve);
-console.log(thirteen);
+// // //- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// let one = 'Hello'
+// let two = 'owu'
+// let three = 'com'
+// let four = 'ua'
+// let five = '1'
+// let six = '10'
+// let seven = '-999'
+// let eight = '123'
+// let Pi = '3.14'
+// let ten = '2.7'
+// let eleven = '16'
+// let twelve = true
+// let thirteen = false
+// // // Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(one);
+// console.log(two);
+// console.log(three);
+// console.log(four);
+// console.log(five);
+// console.log(six);
+// console.log(seven);
+// console.log(eight);
+// console.log(Pi);
+// console.log(ten);
+// console.log(eleven);
+// console.log(twelve);
+// console.log(thirteen);
+//
+// alert(one);
+// alert(two);
+// alert(three);
+// alert(four);
+// alert(five);
+// alert(six);
+// alert(seven);
+// alert(eight);
+// alert(Pi);
+// alert(ten);
+// alert(eleven);
+// alert(twelve);
+// alert(twelve);
+// alert(thirteen);
+//
+// document.write(one);
+// document.write(two);
+// document.write(three);
+// document.write(four);
+// document.write(five);
+// document.write(six);
+// document.write(seven);
+// document.write(eight);
+// document.write(Pi);
+// document.write(ten);
+// document.write(eleven);
+// document.write(twelve);
+// document.write(thirteen);
+// // //- Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
+// one = 'Helloworld';
+// two = 'okten';
+// three = 'ua';
+// four = 'eu';
+// five = 'Spain';
+// six = 'England';
+// seven = 'Scotland';
+// eight = 'Usa';
+// Pi = '3.1454654645';
+// ten = '10';
+// eleven = '11';
+// twelve = false;
+// thirteen = true;
+// // //Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(one);
+// console.log(two);
+// console.log(three);
+// console.log(four);
+// console.log(five);
+// console.log(six);
+// console.log(seven);
+// console.log(eight);
+// console.log(Pi);
+// console.log(ten);
+// console.log(eleven);
+// console.log(twelve);
+// console.log(thirteen);
+//
+// alert(one);
+// alert(two);
+// alert(three);
+// alert(four);
+// alert(five);
+// alert(six);
+// alert(seven);
+// alert(eight);
+// alert(Pi);
+// alert(ten);
+// alert(eleven);
+// alert(twelve);
+// alert(twelve);
+// alert(thirteen);
+//
+// document.write(one);
+// document.write(two);
+// document.write(three);
+// document.write(four);
+// document.write(five);
+// document.write(six);
+// document.write(seven);
+// document.write(eight);
+// document.write(Pi);
+// document.write(ten);
+// document.write(eleven);
+// document.write(twelve);
+// document.write(thirteen);
+// // //- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+// let yourname = prompt('Ваше ім*я?');
+// let fathername = prompt('Ім*я Вашого батька');
+// let UserAge = prompt('Ваш Вік?');
+// // // Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+// console.log(yourname);
+// console.log(fathername);
+// console.log(UserAge);
+//
+// // // Взяти змінні з завдання вище та зєднати їх в одну змінну (конкатинація).
+// let info = yourname + fathername + UserAge;
+// document.write(info);
+// // - За допомогою prompt() отримати 3 числа. Привести їх до числа (Google "String to number JS"). Вивести їх в консоль.
+//
+// let UserAge1 = parseInt(prompt('Скільки тобі років?'));
+// let UserMonth = parseInt(prompt('Місяць народження?'));
+// let UserDate = parseInt(prompt('День народження?'));
+// console.log(UserAge1);
+// console.log(UserMonth);
+// console.log(UserDate);
+// //За допомогою prompt() отримати 4 числа. Привести їх до числа за допомогою paeseInt. Додати їх всі мож собою і результат вивести в консоль.
+// let UserAge2 = parseInt(prompt('Скількі тобі років?'));
+// let UserMonth2 = parseInt(prompt('місяць НАродження?'));
+// let UserDate2 = parseInt(prompt('День народження?'));
+// let FavoriteNumber = parseInt(prompt('Улюблене число?'));
+// console.log(UserAge2 + UserMonth2 + UserDate2 + FavoriteNumber);
+// // // За допомогою prompt() отримати 3 числа з плаваючою точкою за допомогою parseFloat.
+// let UserAge3 = parseFloat(prompt('Скількі тобі років?'));
+// let UserMonth3 = parseFloat(prompt('місяць НАродження?'));
+// let UserDate3 = parseFloat(prompt('День народження?'));
+// console.log(UserAge3);
+// console.log(UserMonth3);
+// console.log(UserDate3);
+// // // Просумувати їх. Отрманий результат записати в змінну result. Вивести цю змінну в консоль.
+// let result = (UserAge3 + UserMonth3 + UserDate3);
+// console.log(result);
+// //- За допомогою prompt() отримати 3 числа з плаваючою точкою. Заокруглити їх (Google "round number JS"). Просумувати їх між собою. Результат вивести в консоль.
+// let Pi10 = parseFloat(prompt('Число Пі?'));
+// let Pi2 = parseFloat(prompt('Пі*2?'));
+// let Pi3 = parseFloat(prompt('Пі*3?'));
+//
+//
+// console.log(Math.round(Pi10 + Pi2 + Pi3));
+//
+//
+// //  - За допомогою prompt() отримати 2 цілих числа.
+// //   Перше число - це число яке будуть підносити до степеня. Друге число - це число яке являється степенню.
+// //   За допомогою Math.pow (Google) піднести перше число в степінь другого числа.
+// //   Example. Число 1 = 5. Число 2 = 2. Результат = 25
+//
+// let Question = prompt("Скільки днів в році?");
+// let Question2 = prompt('Скільки місяців взимку?');
+//
+// console.log(Math.pow(Question, Question2));
+//
+// // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// //   let a = 100; let b = '100'; let c = true;
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+//
+// // - Поставет відповідний оператор в виразах що б вийшов відповідний результат.
+// //   В однакових виразаї не використовувати однакові оператори!!!
+// //     5 ? 6 -> true
+// //     5 ? 6 -> false
+// //     5 ? 6 -> false
+// //     5 ? 6 -> false
+// //     10 ? 10 -> true
+// //     10 ? 10 -> true
+// //     10 ? 10 -> false
+// //     10 ? 10 -> false
+// //     10 ? 10 -> false
+// //     123 ? '123' -> false
+// //     123 ? '123' -> true
+//
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 >= 6);
+// console.log(5 === 6);
+// console.log(10 === 10);
+// console.log(10 >= 10);
+// console.log(10 < 10);
+// console.log(10 > 10);
+// console.log(10 != 10);
+// console.log(123 > 123);
+// console.log(123 === 123);
 
-alert(one);
-alert(two);
-alert(three);
-alert(four);
-alert(five);
-alert(six);
-alert(seven);
-alert(eight);
-alert(Pi);
-alert(ten);
-alert(eleven);
-alert(twelve);
-alert(twelve);
-alert(thirteen);
+// 1. Оголосіть змінні str, num, flag и txt зі значеннями «Привіт», 123, true, «true».
+//    За допомогою typeof переконайтесь, що значення змінних належать типам: string, number и  boolean.
+// let str = 'Привіт'
+// let num = 123
+// let trues = true
+// let truestr = 'true'
+// console.log(typeof str);
+// console.log(typeof  num);
+// console.log(typeof trues);
+// console.log(typeof truestr);
 
-document.write(one);
-document.write(two);
-document.write(three);
-document.write(four);
-document.write(five);
-document.write(six);
-document.write(seven);
-document.write(eight);
-document.write(Pi);
-document.write(ten);
-document.write(eleven);
-document.write(twelve);
-document.write(thirteen);
-// //- Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
-one = 'Helloworld';
-two = 'okten';
-three = 'ua';
-four = 'eu';
-five = 'Spain';
-six = 'England';
-seven = 'Scotland';
-eight = 'Usa';
-Pi = '3.1454654645';
-ten = '10';
-eleven = '11';
-twelve = false;
-thirteen = true;
-// //Вивести кожну змінну за допомогою: console.log , alert, document.write
-console.log(one);
-console.log(two);
-console.log(three);
-console.log(four);
-console.log(five);
-console.log(six);
-console.log(seven);
-console.log(eight);
-console.log(Pi);
-console.log(ten);
-console.log(eleven);
-console.log(twelve);
-console.log(thirteen);
+// 2. Оголосіть змінні a1, a2, a3, a4, a5. За домогою 3х математичних оператцій отримайте числа:
+//     34,
+//     12,
+//     66,
+//     90,
+//     87
+//     Старайтесь використовувати різні оператори.
+//     Example: 88 = (16 / 2) * 11
 
-alert(one);
-alert(two);
-alert(three);
-alert(four);
-alert(five);
-alert(six);
-alert(seven);
-alert(eight);
-alert(Pi);
-alert(ten);
-alert(eleven);
-alert(twelve);
-alert(twelve);
-alert(thirteen);
-
-document.write(one);
-document.write(two);
-document.write(three);
-document.write(four);
-document.write(five);
-document.write(six);
-document.write(seven);
-document.write(eight);
-document.write(Pi);
-document.write(ten);
-document.write(eleven);
-document.write(twelve);
-document.write(thirteen);
-// //- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
-let yourname = prompt('Ваше ім*я?');
-let fathername = prompt('Ім*я Вашого батька');
-let UserAge = prompt('Ваш Вік?');
-// // Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
-console.log(yourname);
-console.log(fathername);
-console.log(UserAge);
-
-// // Взяти змінні з завдання вище та зєднати їх в одну змінну (конкатинація).
-let info = yourname + fathername + UserAge;
-document.write(info);
-// - За допомогою prompt() отримати 3 числа. Привести їх до числа (Google "String to number JS"). Вивести їх в консоль.
-
-let UserAge1 = parseInt(prompt('Скільки тобі років?'));
-let UserMonth = parseInt(prompt('Місяць народження?'));
-let UserDate = parseInt(prompt('День народження?'));
-console.log(UserAge1);
-console.log(UserMonth);
-console.log(UserDate);
-//За допомогою prompt() отримати 4 числа. Привести їх до числа за допомогою paeseInt. Додати їх всі мож собою і результат вивести в консоль.
-let UserAge2 = parseInt(prompt('Скількі тобі років?'));
-let UserMonth2 = parseInt(prompt('місяць НАродження?'));
-let UserDate2 = parseInt(prompt('День народження?'));
-let FavoriteNumber = parseInt(prompt('Улюблене число?'));
-console.log(UserAge2 + UserMonth2 + UserDate2 + FavoriteNumber);
-// // За допомогою prompt() отримати 3 числа з плаваючою точкою за допомогою parseFloat.
-let UserAge3 = parseFloat(prompt('Скількі тобі років?'));
-let UserMonth3 = parseFloat(prompt('місяць НАродження?'));
-let UserDate3 = parseFloat(prompt('День народження?'));
-console.log(UserAge3);
-console.log(UserMonth3);
-console.log(UserDate3);
-// // Просумувати їх. Отрманий результат записати в змінну result. Вивести цю змінну в консоль.
-let result = (UserAge3 + UserMonth3 + UserDate3);
-console.log(result);
-//- За допомогою prompt() отримати 3 числа з плаваючою точкою. Заокруглити їх (Google "round number JS"). Просумувати їх між собою. Результат вивести в консоль.
-let Pi10 = parseFloat(prompt('Число Пі?'));
-let Pi2 = parseFloat(prompt('Пі*2?'));
-let Pi3 = parseFloat(prompt('Пі*3?'));
+// let a1 = 7 * 7 - 15
+// let a2 = 6 * 6 / 3
+// let a3 = 3 + 3 * 21
+// let a4 = 100 - 50 + 40
+// let a5 = 3 * (14 + 15)
+// console.log(a1)
+// console.log(a2)
+// console.log(a3)
+// console.log(a4)
+// console.log(a5)
 
 
-console.log(Math.round(Pi10 + Pi2 + Pi3));
+// let a6 = 5 % 3
+// let a7 = 3 % 5
+// let a8 = 5 + '3'
+// let a9 = '5' - 3
+// let a10 = 75 + 'кг'
+// console.log(a6)
+// console.log(a7)
+// console.log(a8)
+// console.log(a9)
+// console.log(a10)
 
+// 4. Напищіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+//    Значееня площі зберігати в змінній s.
 
-//  - За допомогою prompt() отримати 2 цілих числа.
-//   Перше число - це число яке будуть підносити до степеня. Друге число - це число яке являється степенню.
-//   За допомогою Math.pow (Google) піднести перше число в степінь другого числа.
-//   Example. Число 1 = 5. Число 2 = 2. Результат = 25
+//     let height = 23
+//     let width = 10
+//     let s = height * width
+// console.log(s);
 
-let Question = prompt("Скільки днів в році?");
-let Question2 = prompt('Скільки місяців взимку?');
+// 5.  Напиши код, который находит объем цилиндра высотой 10м (переменная heightC) и диаметром основания 4м (dC),
+//     результат поместите в переменную v.
+// let heightC = 10
+// let dc = 4
+// let dcq = Math.pow(4,2)
+// let v = 3.14 * dcq * heightC
+// console.log(v)
 
-console.log(Math.pow(Question, Question2));
+// 6. У прямоугольного треугольника две стороны n (со значением 3) и m (со значением 4).
+//    Найдите гипотенузу k по теореме Пифагора (нужно использовать функцию Math.pow(число, степень) или оператор возведения в степень ** ).
 
-// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
-//   let a = 100; let b = '100'; let c = true;
-let a = 100;
-let b = '100';
-let c = true;
+// let n = 3
+// let m = 4
+// let k = Math.pow(3, 2) + Math.pow(4, 2)
+// console.log(k)
 
-console.log(typeof a);
-console.log(typeof b);
-console.log(typeof c);
+// 7. Напишите скрипт, который выводит "Hello world", создавши переменную str и выводить спомощью document.write, alert и console.log
 
-// - Поставет відповідний оператор в виразах що б вийшов відповідний результат.
-//   В однакових виразаї не використовувати однакові оператори!!!
-//     5 ? 6 -> true
-//     5 ? 6 -> false
-//     5 ? 6 -> false
-//     5 ? 6 -> false
-//     10 ? 10 -> true
-//     10 ? 10 -> true
-//     10 ? 10 -> false
-//     10 ? 10 -> false
-//     10 ? 10 -> false
-//     123 ? '123' -> false
-//     123 ? '123' -> true
+// let str = 'Hello world'
+//
+// document.write(str)
+// alert(str)
+// console.log(str);
 
-console.log(5 < 6);
-console.log(5 > 6);
-console.log(5 >= 6);
-console.log(5 === 6);
-console.log(10 === 10);
-console.log(10 >= 10);
-console.log(10 < 10);
-console.log(10 > 10);
-console.log(10 != 10);
-console.log(123 > 123);
-console.log(123 === 123);
+// 8. Вывести в окно браузера при помощи метода alert() следующие данные: Ваше ФИО, возраст, хобби (каждой на новой строки спомощью \n).
+// let iAm = 'Skirko Bohdan Yaroslavovych'
+// let age = 30
+// let hobby = 'football'
+// alert(iAm \n age \n hobby)
+
+// 9. Создать 4 переменные с использованием ключевого слова let с именами str1, str2, str3, concatenation.
+//   Переменной str1 присвоить фразу ‘Кто ‘, str2 – ‘ты ‘, str3 – ‘такой?’
+//   Локальной переменной concatenation присвоить результат конкатенации 3-х строк: str1, str2, str3.
+//   Вывести в документ содержимое переменной concatenation спомощью document.write
+
+// let str1 = 'Хто'
+// let str2 = 'ти'
+// let str3 = 'такий'
+// let concatenation = str1 + str2 + str3
+// document.write(concatenation)
+//
+// let str = "20";
+//      let a = 5;
+//      document.write(str + a + "<br/>");
+//      document.write(str - a + "<br/>");
+//      document.write(str * "2" + "<br/>");
+//      document.write(str / 2 + "<br/>");
+
+  // let str = prompt("Enter something", "ho-ho")
+  //   console.log(str);
+// 13. С помощью окна ввода, вызываемого методом prompt, сделать сложение двух чисел, а вывод результата при помощи метода alert
+// let firstNum = prompt('1 число')
+// let secondNum = prompt('2 число')
+// alert(firstNum + secondNum)
+// 14. С помощью окна ввода, вызываемого методом prompt, пользователь последовательно выводит имя, фамилию и возраст, а вам не обходимо вывести строку такого вида
+// //       Доброго вечера Иван Иванов, мои поздравления что вам 32 , а вывод результата при помощи метода alert
+//     let name = prompt("Ваше ім'я?")
+//     let surName = prompt('Ваше призвіще')
+//     let age = prompt('Скільки Вам років?')
+// alert(`Доброго вечора ${name} ${surName}  Мої вітання Вам ${age}`)
+
+// 1. Три різних числа вводяться через prompt().
+// За допомоги if else вивести іх в порядку зростання. (відсортувати по зростанню)
+
+// Let a = prompt('Вік?')
+// let b = prompt('Рік')
+// Let c =prompt('число')
+//
+// if (a>b){console.log(a)}
+//  else b>a console.log(b)
+//  if (b>c) {console.log(b)}
+//  else (c>b) {console.log(c)}
